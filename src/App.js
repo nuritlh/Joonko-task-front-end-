820812febd5aaa2b34ec3d9f75f3b29e293b6daf
Nurit Levi <nuritlh@gmail.com>
2020-10-01 15:38:46 +0300

    test to git

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+//test to get
 function App() {
   return (
     <div className="App-wrapper">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
